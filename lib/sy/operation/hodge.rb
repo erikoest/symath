0d5ec4d5045b6d4750adb57447b821bed03d6b9c
erikoest <erik@uninett.no>
2019-07-03 08:37:38 +0200

    Bugfix hodge star, corrected mapping from scalar to pseudoscalar.

--- lib/sy/operation/hodge.rb
+++ lib/sy/operation/hodge.rb
@@ -8,27 +8,18 @@ module Sy
     end
     
     def act(exp)
-      # Replace nvectors and nforms with their hodge dual
-
-      # Product expressions are split into a scalar part which is kept and a vector part which
-      # is replaced with its hodge dual
-      if exp.is_prod_exp?
-        s = exp.scalar_factors_exp
-        c = exp.coefficient.to_m
-        dc = exp.div_coefficient.to_m
-        h = Sy::Variable.hodge_dual(exp.vector_factors_exp)
-
-        return exp.sign.to_m.mult(c.mult(s).div(dc)).mult(h)
-      end
-
       # Recurse down sums and subtractions
       if exp.is_sum_exp?
         act_subexpressions(exp)
         return exp
+      else
+        # Replace nvectors and nforms with their hodge dual
+        s = exp.scalar_factors_exp
+        c = exp.coefficient.to_m
+        dc = exp.div_coefficient.to_m
+        h = Sy::Variable.hodge_dual(exp.vector_factors_exp)
+        return exp.sign.to_m.mult(c.mult(s).div(dc)).mult(h)
       end
-
-      # Other operators and functions are ignored
-      return exp
     end
   end
 end
